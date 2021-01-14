5ecf5711f9137b4ceaa82131dce53afbe23b87c8
Bandon Tseng <bdfish1104@gmail.com>
2021-01-14 06:05:20 -0700

    Filter unknown contact messages
    Move all unknown contact messages to archive.

--- domain/src/main/java/com/moez/QKSMS/interactor/ReceiveSms.kt
+++ domain/src/main/java/com/moez/QKSMS/interactor/ReceiveSms.kt
@@ -26,18 +26,22 @@ import com.moez.QKSMS.manager.ShortcutManager
 import com.moez.QKSMS.repository.ConversationRepository
 import com.moez.QKSMS.repository.MessageRepository
 import com.moez.QKSMS.util.Preferences
+import com.moez.QKSMS.repository.ContactRepository
 import io.reactivex.Flowable
 import timber.log.Timber
 import javax.inject.Inject
 
+import java.util.NoSuchElementException
+
 class ReceiveSms @Inject constructor(
-    private val conversationRepo: ConversationRepository,
-    private val blockingClient: BlockingClient,
-    private val prefs: Preferences,
-    private val messageRepo: MessageRepository,
-    private val notificationManager: NotificationManager,
-    private val updateBadge: UpdateBadge,
-    private val shortcutManager: ShortcutManager
+        private val conversationRepo: ConversationRepository,
+        private val blockingClient: BlockingClient,
+        private val prefs: Preferences,
+        private val messageRepo: MessageRepository,
+        private val notificationManager: NotificationManager,
+        private val updateBadge: UpdateBadge,
+        private val shortcutManager: ShortcutManager,
+        private val contactRepository: ContactRepository
 ) : Interactor<ReceiveSms.Params>() {
 
     class Params(val subId: Int, val messages: Array<SmsMessage>)
@@ -51,7 +55,20 @@ class ReceiveSms @Inject constructor(
                     val address = messages[0].displayOriginatingAddress
                     val action = blockingClient.getAction(address).blockingGet()
                     val shouldDrop = prefs.drop.get()
-                    Timber.v("block=$action, drop=$shouldDrop")
+
+                    /*
+                    val queryResult = contactRepository.findContactUri(address)
+
+                    var inContactsRepo: Boolean = false
+                    try {
+                        val uri = queryResult.blockingGet()
+                        inContactsRepo = true;
+                        Timber.v("uri=$uri")
+                    } catch (ex: java.util.NoSuchElementException) {
+
+                    }
+                    Timber.v("address=$address queryResult=$inContactsRepo")
+                    */
 
                     // If we should drop the message, don't even save it
                     if (action is BlockingClient.Action.Block && shouldDrop) {
@@ -75,20 +92,81 @@ class ReceiveSms @Inject constructor(
                         else -> Unit
                     }
 
+                    /*
+                    if (!inContactsRepo) {
+                        Timber.v("markedArchived")
+                        conversationRepo.markArchived(message.threadId)
+                    }
+                    */
+
+                    Timber.v("message1 = $message")
+
                     message
                 }
                 .doOnNext { message ->
-                    conversationRepo.updateConversations(message.threadId) // Update the conversation
+                    Timber.v("message2 = $message")
+                    val msg = conversationRepo.updateConversations(message.threadId) // Update the conversation
+                    Timber.v("message2 = $msg")
+                    msg
                 }
                 .mapNotNull { message ->
-                    conversationRepo.getOrCreateConversation(message.threadId) // Map message to conversation
+                    Timber.v("message3 = " + message.address.toString())
+
+                    // This block will create new conversation from conversation repo.
+                    // So we have to check whether the address is in contact list.
+                    val queryResult = contactRepository.findContactUri(message.address)
+                    var inContactsRepo: Boolean = false
+                    try {
+                        val uri = queryResult.blockingGet()
+                        inContactsRepo = true;
+                    } catch (ex: java.util.NoSuchElementException) {
+
+                    }
+
+                    // Check if the conversation exists in repo:
+                    // If it does not exist in repo and the address is unknown, we will archived it.
+                    // Otherwise, the address is in contact list or this is not a new conversation, we do not change its archived status.
+                    // Therefore, once we unarchived a message whose address is not in contact list, we can receive new messages
+                    // and get update notification.
+                    val checkMsg = conversationRepo.getConversation(message.threadId)
+
+                    Timber.v("message 3 " + checkMsg?.toString())
+
+                    if(!inContactsRepo && checkMsg == null) {
+                        // The address does not exist in conact list and this is a new conversation.
+                        Timber.v("message3 markArchived")
+
+                        // Because we cannot modify the message directly which will cause Exception.
+                        // We create the conversation and then mark it as archived.
+                        // Before return the message, we get the message again - the new message object will contain the latest archived state.
+                        val tempMsg = conversationRepo.getOrCreateConversation(message.threadId)
+                        conversationRepo.markArchived(message.threadId)
+                    }
+
+                    val msg = conversationRepo.getOrCreateConversation(message.threadId)
+
+                    Timber.v("message3 = " + msg?.archived)
+                    msg
                 }
-                .filter { conversation -> !conversation.blocked } // Don't notify for blocked conversations
+                .filter { conversation ->
+                    Timber.v("conversation1 = $conversation")
+                    if(conversation.archived) {
+                        Timber.v("conversation1 archived")
+                    }
+                    else {
+                        Timber.v("conversation1 NOT archived")
+                    }
+                    !conversation.blocked && !conversation.archived
+                } // Don't notify for blocked conversations
                 .doOnNext { conversation ->
+                    Timber.v("test before check archived")
                     // Unarchive conversation if necessary
-                    if (conversation.archived) conversationRepo.markUnarchived(conversation.id)
+                    // if (conversation.archived) conversationRepo.markUnarchived(conversation.id)
                 }
-                .map { conversation -> conversation.id } // Map to the id because [delay] will put us on the wrong thread
+                .map { conversation ->
+                    Timber.v("conversation3 = $conversation")
+                    conversation.id
+                } // Map to the id because [delay] will put us on the wrong thread
                 .doOnNext { threadId -> notificationManager.update(threadId) } // Update the notification
                 .doOnNext { shortcutManager.updateShortcuts() } // Update shortcuts
                 .flatMap { updateBadge.buildObservable(Unit) } // Update the badge and widget
